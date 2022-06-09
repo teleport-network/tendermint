9a18ed3ca6b784635dac39296e2fdd1f3bf82ee0
M. J. Fromberger <fromberger@interchain.io>
2022-06-09 11:10:52 -0700

    Log pointers

--- internal/mempool/v1/repro_test.go
+++ internal/mempool/v1/repro_test.go
@@ -3,17 +3,39 @@ package v1
 import (
 	"crypto/rand"
 	"flag"
+	"fmt"
+	"os"
+	"runtime"
 	"testing"
 	"time"
 
 	"github.com/tendermint/tendermint/types"
 )
 
-var txLimit = flag.Int("num-txn", 1, "Number of transactions")
+var (
+	txLimit = flag.Int("num-txn", 1, "Number of transactions")
+	ptrLog  = flag.String("logfile", "pointer.log", "Pointer log")
+)
 
 func TestMempoolAddRemove(t *testing.T) {
 	txmp := setup(t, *txLimit)
 	txch := make(chan *WrappedTx, 10)
+
+	f, err := os.Create(*ptrLog)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			t.Errorf("Close pointer log: %v", err)
+		}
+	}()
+
+	m := make([]byte, 1<<20)
+	fmt.Fprintf(f, "* %p\n", &m)
+	runtime.SetFinalizer(&m, func(m *[]byte) {
+		fmt.Fprintf(f, "x %p\n", m)
+	})
 
 	go func() {
 		defer close(txch)
@@ -28,6 +50,11 @@ func TestMempoolAddRemove(t *testing.T) {
 				timestamp: time.Now().UTC(),
 				height:    txmp.height,
 			}
+			fmt.Fprintf(f, "+ %p\n", wtx)
+			runtime.SetFinalizer(wtx, func(w *WrappedTx) {
+				fmt.Fprintf(f, "- %p\n", w)
+			})
+
 			txmp.insertTx(wtx)
 			txch <- wtx
 		}
@@ -35,4 +62,6 @@ func TestMempoolAddRemove(t *testing.T) {
 	for tx := range txch {
 		txmp.removeTx(tx, true)
 	}
+
+	runtime.GC()
 }
